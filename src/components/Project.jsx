fb53d7a474bd23b09ce1ee26b19123b42b3dcc0e
HiteshYasam <hiteshyasam@gmail.com>
2024-07-31 15:14:10 +0530

    fix: some bug

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import INFO from './myInfo';
 import "./Project.css"
 import { FaGithub } from "react-icons/fa";
+import portfolioImg from "/assets/portfolioImg.png";
+import todoImg from "/assets/todoImg.png";
+import spotifyImg from "/assets/spotifyImg.png"
+import twitterImg from "/assets/twitterImg.png"
+import netflixImg from "/assets/netflixImg.png"
+import landingpageImg from "/assets/landingpageImg.png"
+
+const images = [portfolioImg, todoImg, spotifyImg, twitterImg, netflixImg, landingpageImg]
 
 const Project = () => {
   return (
@@ -13,7 +21,7 @@ const Project = () => {
         {INFO.projectsec.projects.map((project, index)=>{
             return (
                 <div className="project" key={index}>
-                    <img src={project.image}alt="project-pic" />
+                    <img src={images[index]}alt="project-pic" />
                     <h3>{project.title} <span></span></h3>
                     <p className='projSkill'>{project.skills}</p>
                     <p>{project.description}</p>
